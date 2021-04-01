6bcaecd016da7b777df401ef18865aba36e0a455
Keanu Nichols <nichols.keanu9@gmail.com>
2021-04-01 11:39:36 -0400

    Add files via upload

---
+++ testing/make_test.py
@@ -0,0 +1,17 @@
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+
+
+ground_truth_images_folder = 'testing_output_images/'
+
+files = [f for f in listdir(ground_truth_images_folder) if isfile(join(ground_truth_images_folder, f))]
+
+training_image_names = pd.DataFrame(columns=['name'])
+for file_ in range(1, len(files)+1):
+    name = str(file_)
+    training_image_names = training_image_names.append({'name': name}, ignore_index=True)
+
+print(training_image_names)
+training_image_names.to_csv('test.csv',index=False)
+
---
+++ testing/partition_training_image_full_color.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import asarray
+from PIL import Image
+import os
+from os import listdir
+from os.path import isfile, join
+
+def split_channels(file_, output_file, file_num):
+    image = Image.open(file_)
+    output_image = Image.open(output_file)
+    array = asarray(image)
+    out_array = asarray(output_image)
+
+
+    patch_size = 384
+
+    #channels = ['train_red', 'train_green', 'train_blue']
+    channels = ['testing_full_color_images', 'testing_output_images']
+    #color = ['red', 'green', 'blue']
+    color = ['full_color']
+    for channel in range(1):
+        width_after = patch_size
+        num = file_num
+        #num = 1 
+        for width in range(0, image.size[0], patch_size):
+            height_after = patch_size
+            for height in range(0, image.size[1], patch_size):
+                #print(height)
+                patch = array[height:height_after,width:width_after, :]
+                out_patch = out_array[height:height_after,width:width_after]  
+                height_img = patch[:,:,0].shape[0]
+                if(height_img % patch_size != 0):
+                    height_after+=patch_size
+                    continue
+                width_img = patch[:,:,0].shape[1] 
+                if(height_img != 0 and width_img !=0):
+                    if((np.count_nonzero(patch[:,:,0] == 255) / (height_img*width_img)) > 0.01):
+                        height_after+=patch_size
+                        continue 
+                patch = Image.fromarray(patch)
+                output_patch = Image.fromarray(out_patch)
+                patch.save(channels[channel] + '/' + color[channel] + '_testing_' + str(num) + '.TIF')
+                output_patch.save(channels[channel+1] + '/' + 'testing_output_' + str(num) + '.TIF')
+                #plt.savefig('../training/train_gt/ground_truth_' + str(num) + '.tif')
+                num+=1
+                if(num%1000 == 0):
+                    print(num)
+                height_after+=patch_size
+                if(height_after > image.size[1]):
+                    break
+
+            width_after+=patch_size
+            if(width_after > image.size[0]):
+                break
+    return num
+
+
+#full_color_images_folder = 'full_color_images/'
+full_color_images_folder = 'input/'
+output_images_folder = 'output/'
+files = [f for f in listdir(full_color_images_folder) if isfile(join(full_color_images_folder, f))]
+files_ = ['img-' + str(num) + '.tiff' for num in range(1,len(files)+1)]
+file_num = 1
+#folders = ['train_red', 'train_green', 'train_blue']
+folders = ['testing_full_color_images', 'testing_output_images']
+files.sort()
+print(files_)
+
+for folder in folders:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+for file_ in files_:
+    training_img = full_color_images_folder + file_
+    output_img = output_images_folder + file_
+    file_num = split_channels(training_img, output_img, file_num) 
+    #if(file_num > 445):
+    #    break
+
